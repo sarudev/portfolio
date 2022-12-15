705e1d92f3baf7da62441e3d6840158e4ae46700
José María Coria <ExB8Ch@gmail.com>
2022-12-15 00:30:34 -0300

    Initial commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Lines = styled.div``
+const Line =   styled.div``
+
+function App                  () {
+  return (
+    <Lines>
+      <Line>{"<Sarudev"}</Line>
+      <Line>{  "realName=\"Jose Coria\""}</Line>
+      <Line>{  "country=\"Argentina\""}</Line>
+      <Line>{  "age={19}"}</Line>
+      <Line>{"/>"}</Line>
+    </Lines>
+  )
+}
+
+export default App
+
+
