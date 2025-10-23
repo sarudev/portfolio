975afc81ec47dd112e960fc52159b35b2d9855e0
sarudev <josecoria959@gmail.com>
2025-10-23 01:05:53 -0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+})
